7787ad6a651386b5df823f36b4aec16f709e3803
anzhelaiondem <anzhlela.iondem.fd@gmail.com>
2021-05-18 00:07:07 +0400

    Add main() function with ngrok.
    Create main() function and move everything from "if __name__ == "__main__":" to main().
    Add Ngrok connection in the main().

--- server.py
+++ server.py
@@ -2,11 +2,15 @@ import websockets
 import asyncio
 import ephem
 
+from pyngrok import ngrok
+
+PORT = 5000
+
 
 async def send_msg(websocket, path):
     while True:
         await websocket.send(calculating_ra_dec_of_moon())
-        await asyncio.sleep(10)
+        await asyncio.sleep(1)
 
 
 def calculating_ra_dec_of_moon():
@@ -15,7 +19,13 @@ def calculating_ra_dec_of_moon():
     return f"RA: {m.ra}, DEC: {m.dec}"
 
 
-if __name__ == "__main__":
-    start_server = websockets.serve(send_msg, "localhost", 80)
+def main():
+    url = ngrok.connect(PORT).public_url
+    print("Ngrok tunnel address is: \"{}\" -> \"http://127.0.0.1:{}\"".format(url, PORT))
+    start_server = websockets.serve(send_msg, "localhost", PORT)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    main()
