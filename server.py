cdf38f3508236b9c625ee83857d09d6d28e37d29
anzhelaiondem <anzhlela.iondem.fd@gmail.com>
2021-05-24 23:48:17 +0400

    Add logging. Handle errors with try, except.
    Logging (level=INFO) creates file with connected and disconnected clients, also gives actual number of connected clients. The same info is printed in terminal too.
    Try, except handle disconnections or closings of clients.

--- server.py
+++ server.py
@@ -1,28 +1,43 @@
 import websockets
 import asyncio
 import ephem
+import logging
 
 from pyngrok import ngrok
 
-PORT = 5000
+HOST = "localhost"
+PORT = 5050
+USERS = set()
+
+logging.basicConfig(filename='client.log', filemode='w', level=logging.INFO, datefmt='%m-%d %H:%M:%S',
+                    format=' %(levelname)-8s %(asctime)s - %(message)s')
 
 
-async def send_msg(websocket, path):
-    while True:
-        await websocket.send(calculating_ra_dec_of_moon())
-        await asyncio.sleep(1)
+async def handler(websocket, path):
+    USERS.add(websocket.remote_address[:2])
+    print(f"Client {websocket.remote_address[:2]} is connected. Active clients number: {len(USERS)}")
+    logging.info(f"Client {websocket.remote_address[:2]} is connected. Active clients number: {len(USERS)}")
+    try:
+        while True:
+            msg = await calculating_ra_dec_of_moon()
+            await websocket.send(msg)
+            await asyncio.sleep(10)
+    except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK):
+        print(f"Client {websocket.remote_address[:2]} is disconnected. Active clients number: {len(USERS) - 1}")
+        logging.info(f"Client {websocket.remote_address[:2]} is disconnected. Active clients number: {len(USERS) - 1}")
+        USERS.remove(websocket.remote_address[:2])
 
 
-def calculating_ra_dec_of_moon():
+async def calculating_ra_dec_of_moon():
     m = ephem.Moon()
     m.compute()
     return f"RA: {m.ra}, DEC: {m.dec}"
 
 
 def main():
-    url = ngrok.connect(PORT).public_url
-    print("Ngrok tunnel address is: \"{}\" -> \"http://127.0.0.1:{}\"".format(url, PORT))
-    start_server = websockets.serve(send_msg, "localhost", PORT)
+    ng_url = ngrok.connect(PORT).public_url
+    print("Ngrok tunnel address: ws{}".format(ng_url[4:], PORT))
+    start_server = websockets.serve(handler, "localhost", PORT)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
